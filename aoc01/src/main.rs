8c4cbbc8287d1327833c1e45a571b7792eaf8f4c
livexia <istillalive@live.cn>
2023-12-01 14:24:40 +0800

    trim code for day01

--- aoc01/src/main.rs
+++ aoc01/src/main.rs
@@ -13,80 +13,63 @@ fn main() -> Result<()> {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
 
-    let mut sum = 0;
-    for line in input.lines() {
-        let mut digits = vec![];
-        let raw_chars: Vec<char> = line.chars().collect();
-
-        let mut chars = &raw_chars[..];
-        while let (Some(remain_chars), digit_opt) = find_digit_with_letters(&chars) {
-            if let Some(digit) = digit_opt {
-                digits.push(digit);
-            }
-            chars = remain_chars;
-        }
-        sum += digits[0] * 10 + digits.last().unwrap();
-    }
-
-    writeln!(io::stdout(), "Part 2: {}", sum)?;
-
     part1(&input)?;
-    // part2()?;
+    part2(&input)?;
     Ok(())
 }
 
-fn find_digit_with_letters(chars: &[char]) -> (Option<&[char]>, Option<u32>) {
+fn match_digit_with_letters(chars: &[char]) -> Option<(u32, usize)> {
     if chars.is_empty() {
-        return (None, None);
+        return None;
     }
     let l = chars.len();
     match chars[0] {
         'o' => {
-            if 3 <= l && chars[1] == 'n' && chars[2] == 'e' {
-                return (Some(&chars[1..]), Some(1));
+            if 3 <= l && chars[..3] == ['o', 'n', 'e'] {
+                return Some((1, 3));
             }
         }
         't' => {
-            if 3 <= l && chars[1] == 'w' && chars[2] == 'o' {
-                return (Some(&chars[1..]), Some(2));
+            if 3 <= l && chars[..3] == ['t', 'w', 'o'] {
+                return Some((2, 3));
             }
-            if 5 <= l && chars[0..5] == ['t', 'h', 'r', 'e', 'e'] {
-                return (Some(&chars[1..]), Some(3));
+            if 5 <= l && chars[..5] == ['t', 'h', 'r', 'e', 'e'] {
+                return Some((3, 5));
             }
         }
         'f' => {
-            if 4 <= l && chars[0..4] == ['f', 'o', 'u', 'r'] {
-                return (Some(&chars[1..]), Some(4));
+            if 4 <= l && chars[..4] == ['f', 'o', 'u', 'r'] {
+                return Some((4, 4));
             }
-            if 4 <= l && chars[0..4] == ['f', 'i', 'v', 'e'] {
-                return (Some(&chars[1..]), Some(5));
+            if 4 <= l && chars[..4] == ['f', 'i', 'v', 'e'] {
+                return Some((5, 4));
             }
         }
         's' => {
-            if 3 <= l && chars[0..3] == ['s', 'i', 'x'] {
-                return (Some(&chars[1..]), Some(6));
+            if 3 <= l && chars[..3] == ['s', 'i', 'x'] {
+                return Some((6, 3));
             }
             if 5 <= l && chars[..5] == ['s', 'e', 'v', 'e', 'n'] {
-                return (Some(&chars[1..]), Some(7));
+                return Some((7, 5));
             }
         }
         'e' => {
             if 5 <= l && chars[..5] == ['e', 'i', 'g', 'h', 't'] {
-                return (Some(&chars[1..]), Some(8));
+                return Some((8, 5));
             }
         }
         'n' => {
             if 4 <= l && chars[..4] == ['n', 'i', 'n', 'e'] {
-                return (Some(&chars[1..]), Some(9));
+                return Some((9, 4));
             }
         }
         c => {
             if let Some(n) = c.to_digit(10) {
-                return (Some(&chars[1..]), Some(n));
+                return Some((n, 1));
             }
         }
     }
-    return (Some(&chars[1..]), None);
+    None
 }
 
 fn part1(input: &str) -> Result<()> {
@@ -94,22 +77,45 @@ fn part1(input: &str) -> Result<()> {
 
     let mut sum = 0;
     for line in input.lines() {
-        let first_digit = match line.chars().find(|c| c.is_numeric()) {
-            None => return err!("nuable to find the first digit for the calibration value"),
-            Some(c) => c.to_digit(10),
-        }
-        .unwrap();
-        let last_digit = line
-            .chars()
-            .rev()
-            .find(|c| c.is_numeric())
-            .unwrap()
-            .to_digit(10)
-            .unwrap();
+        let first_digit = match line.chars().find_map(|c| c.to_digit(10)) {
+            None => return err!("unable to find the first digit for the calibration value"),
+            Some(c) => c,
+        };
+        let last_digit = match line.chars().rev().find_map(|c| c.to_digit(10)) {
+            None => return err!("unable to find the first digit for the calibration value"),
+            Some(c) => c,
+        };
         sum += first_digit * 10 + last_digit;
     }
 
     writeln!(io::stdout(), "Part 1: {}", sum)?;
+    writeln!(io::stdout(), "> Time elapsed is: {:?}", start.elapsed())?;
+    Ok(())
+}
+
+fn part2(input: &str) -> Result<()> {
+    let start = Instant::now();
+
+    let mut sum = 0;
+    for line in input.lines() {
+        let mut first_digit = 0;
+        let mut last_digit = 0;
+        let chars: Vec<char> = line.chars().collect();
+        for i in 0..chars.len() {
+            match match_digit_with_letters(&chars[i..]) {
+                Some((digit, _)) => {
+                    if first_digit == 0 {
+                        first_digit = digit
+                    }
+                    last_digit = digit;
+                }
+                _ => (),
+            }
+        }
+
+        sum += first_digit * 10 + last_digit;
+    }
+    writeln!(io::stdout(), "Part 2: {}", sum)?;
     writeln!(io::stdout(), "> Time elapsed is: {:?}", start.elapsed())?;
     Ok(())
 }
