66aed6fb11134980d10b6debde7c5503a061da51
livexia <istillalive@live.cn>
2023-12-05 15:20:32 +0800

    day 5 part 2 with wrong solution

--- aoc05/src/main.rs
+++ aoc05/src/main.rs
@@ -10,7 +10,9 @@ macro_rules! err {
 type Result<T> = ::std::result::Result<T, Box<dyn Error>>;
 
 type Number = i64;
+
 type SingleMap = (Number, Number, Number); // dest, src, length
+type Range = (Number, Number); // [start, end)
 
 #[derive(Debug)]
 struct Almanac {
@@ -98,31 +100,96 @@ fn part1(almanac: &Almanac) -> Result<Number> {
     Ok(result)
 }
 
+fn convert_range(
+    input: Range,
+    dest: Number,
+    src: Number,
+    length: Number,
+) -> (Vec<Range>, Option<Range>) {
+    // src range: src..src+length
+    // src and dest offset is: dest - src
+    // then: dest = src + offset
+    let offset = dest - src;
+
+    let src_end = src + length;
+    let (start, end) = input;
+    if end <= src || src_end <= start {
+        return (vec![input], None);
+    } else {
+        let overlaps = Some((start.max(src) + offset, end.min(src_end) + offset));
+        // Number::MIN..src, src_end..Number::MAX
+        let remain_range: Vec<Range> = [(start, end.min(src)), (start.max(src_end), end)]
+            .into_iter()
+            .filter(|(a, b)| a < b)
+            .collect();
+        println!(
+            "input: {:?}\nover: {:?}\nremain: {:?}",
+            input, overlaps, remain_range
+        );
+        (remain_range, overlaps)
+    }
+}
+
+// this funtion only works when a.0 <= b.0
+fn merge_range(a: Range, b: Range) -> (Range, Option<Range>) {
+    let a_end = a.1;
+    let b_start = b.0;
+    if a_end >= b_start {
+        ((a.0, b.1), None)
+    } else {
+        (a, Some(b))
+    }
+}
+
+fn convert_range_with_maps(range: Range, maps: &[SingleMap], converted: &mut Vec<Range>) {
+    if maps.is_empty() {
+        converted.push(range);
+    }
+    let (dest, src, length) = maps[0];
+    let (r_ranges, overlaps) = convert_range(range, dest, src, length);
+    if let Some(overlaps) = overlaps {
+        converted.push(overlaps);
+    }
+    for r in r_ranges {
+        convert_range_with_maps(r, &maps[1..], converted);
+    }
+}
+
 fn part2(almanac: &Almanac) -> Result<Number> {
     let start = Instant::now();
 
-    let result = almanac
+    let mut ranges: Vec<Range> = almanac
         .seeds
         .chunks(2)
-        .map(|seed_range| {
-            (seed_range[0]..seed_range[0] + seed_range[1])
-                .map(|seed| {
-                    let mut start = seed;
-                    for maps in &almanac.maps {
-                        if let Some(next) = maps
-                            .iter()
-                            .find_map(|&(dest, src, length)| convert(start, dest, src, length))
-                        {
-                            start = next
-                        }
-                    }
-                    start
-                })
-                .min()
-                .unwrap()
-        })
-        .min()
-        .unwrap();
+        .map(|chunks| (chunks[0], chunks[0] + chunks[1]))
+        .collect();
+    ranges.sort();
+    for maps in &almanac.maps {
+        let mut next_ranges = vec![];
+        for &range in &ranges {
+            let mut remain_ranges = vec![];
+            let mut flag = false;
+            for map in maps {
+                let &(dest, src, length) = map;
+                let (r_ranges, overlaps) = convert_range(range, dest, src, length);
+                if let Some(overlaps) = overlaps {
+                    next_ranges.push(overlaps);
+                    remain_ranges.extend_from_slice(&r_ranges);
+                    flag = true;
+                };
+            }
+            if !flag {
+                next_ranges.push(range);
+                assert_eq!(remain_ranges.is_empty(), true);
+            }
+            next_ranges.extend_from_slice(&remain_ranges);
+        }
+        next_ranges.sort();
+
+        std::mem::swap(&mut ranges, &mut next_ranges);
+    }
+
+    let result = ranges.into_iter().map(|(s, _)| s).min().unwrap();
 
     writeln!(io::stdout(), "Part 2: {result}")?;
     writeln!(io::stdout(), "> Time elapsed is: {:?}", start.elapsed())?;
