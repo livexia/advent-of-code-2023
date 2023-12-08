f1249a71f09e5a47a6c799c44fee783d4df09a26
livexia <istillalive@live.cn>
2023-12-08 18:12:25 +0800

    day 8 part 2 with dfs and cycle detection

--- aoc08/src/main.rs
+++ aoc08/src/main.rs
@@ -10,10 +10,10 @@ macro_rules! err {
 
 type Result<T> = ::std::result::Result<T, Box<dyn Error>>;
 
-type NodeId = usize;
-type NextNode = (usize, usize);
+type Network = HashMap<usize, (usize, usize)>;
+type NodeMap = HashMap<String, usize>;
 
-fn set_node_id(name: &str, map: &mut HashMap<String, NodeId>, last_id: &mut usize) -> NodeId {
+fn set_node_id(name: &str, map: &mut NodeMap, last_id: &mut usize) -> usize {
     if let Some(id) = map.get(name) {
         *id
     } else {
@@ -23,13 +23,8 @@ fn set_node_id(name: &str, map: &mut HashMap<String, NodeId>, last_id: &mut usiz
     }
 }
 
-fn parse_input(
-    input: &str,
-) -> Result<(
-    Vec<char>,
-    HashMap<NodeId, NextNode>,
-    HashMap<String, NodeId>,
-)> {
+fn parse_input(input: &str) -> Result<(Vec<char>, Network, NodeMap)> {
+    let input = input.replace([')', '('], "");
     let mut lines = input.lines().filter(|l| !l.trim().is_empty());
     // parse instructions as chars;
     let instrs: Vec<_> = if let Some(line) = lines.next() {
@@ -43,11 +38,9 @@ fn parse_input(
     for line in lines {
         if let Some((node_name, node_next)) = line.split_once('=') {
             let id = set_node_id(node_name.trim(), &mut map, &mut last_id);
-            if let Some((left, right)) = node_next.trim_matches(['(', ')', ' ']).split_once(',') {
-                let left_id =
-                    set_node_id(left.trim_matches(['(', ')', ' ']), &mut map, &mut last_id);
-                let right_id =
-                    set_node_id(right.trim_matches(['(', ')', ' ']), &mut map, &mut last_id);
+            if let Some((left, right)) = node_next.trim().split_once(',') {
+                let left_id = set_node_id(left.trim(), &mut map, &mut last_id);
+                let right_id = set_node_id(right.trim(), &mut map, &mut last_id);
                 network.insert(id, (left_id, right_id));
             } else {
                 return err!("Unable to parse nodes left and right: {:?}", line);
@@ -59,41 +52,77 @@ fn parse_input(
     Ok((instrs, network, map))
 }
 
-fn move_node(instr: char, id: NodeId, network: &HashMap<NodeId, NextNode>) -> Result<NodeId> {
-    match (instr, network.get(&id)) {
-        ('R', Some((_, r))) => Ok(*r),
-        ('L', Some((l, _))) => Ok(*l),
-        _ => err!("Unable to move from {id}"),
-    }
+fn next_node(start: (usize, usize), instrs: &[char], network: &Network) -> Result<(usize, usize)> {
+    Ok(match (instrs[start.1], network.get(&start.0)) {
+        ('R', Some((_, r))) => (*r, (start.1 + 1) % instrs.len()),
+        ('L', Some((l, _))) => (*l, (start.1 + 1) % instrs.len()),
+        _ => return err!("Unable to move with {start:?}"),
+    })
 }
 
+// see: https://en.wikipedia.org/wiki/Cycle_detection#Floyd's_tortoise_and_hare
+fn cycle_detect(start: usize, instrs: &[char], network: &Network) -> Result<usize> {
+    let mut tortoise = next_node((start, 0), instrs, network)?;
+    let mut hare = next_node(tortoise, instrs, network)?;
+    while tortoise != hare {
+        tortoise = next_node(tortoise, instrs, network)?;
+
+        hare = next_node(hare, instrs, network)?;
+        hare = next_node(hare, instrs, network)?;
+    }
+
+    let mut mu = 0;
+    tortoise = (start, 0);
+    while tortoise != hare {
+        tortoise = next_node(tortoise, instrs, network)?;
+
+        hare = next_node(hare, instrs, network)?;
+
+        mu += 1;
+    }
+
+    let mut lam = 1;
+    hare = next_node(tortoise, instrs, network)?;
+    while tortoise != hare {
+        hare = next_node(hare, instrs, network)?;
+
+        lam += 1;
+    }
+
+    Ok(mu + lam)
+}
+
+// search until cycle
 fn get_steps(
-    a: usize,
-    b: usize,
+    start: usize,
+    end_ids: &[usize],
     instrs: &[char],
-    network: &HashMap<NodeId, NextNode>,
-) -> Result<usize> {
-    let mut cur = a;
+    network: &Network,
+) -> Result<Vec<(usize, usize)>> {
+    let mut cur = start;
     let mut cur_instr = 0;
+
+    let mut ends = vec![];
     let mut steps = 0;
-    let mut seen = HashSet::new();
-    while cur != b && seen.insert((cur, cur_instr)) {
-        cur = move_node(instrs[cur_instr], cur, network)?;
+
+    let limit = cycle_detect(start, instrs, network)?;
+
+    for _ in 0..limit {
+        if end_ids.contains(&cur) {
+            ends.push((cur, steps))
+        }
+        cur = match (instrs[cur_instr], network.get(&cur)) {
+            ('R', Some((_, r))) => *r,
+            ('L', Some((l, _))) => *l,
+            _ => return err!("Unable to move from {cur}"),
+        };
         steps += 1;
         cur_instr = (cur_instr + 1) % instrs.len();
     }
-    if cur == b {
-        Ok(steps)
-    } else {
-        err!("There is a loop")
-    }
+    Ok(ends)
 }
 
-fn part1(
-    instrs: &[char],
-    network: &HashMap<NodeId, NextNode>,
-    map: &HashMap<String, NodeId>,
-) -> Result<usize> {
+fn part1(instrs: &[char], network: &Network, map: &NodeMap) -> Result<usize> {
     let start = Instant::now();
 
     let &start_id = map
@@ -103,7 +132,7 @@ fn part1(
         .get("ZZZ")
         .expect("Unable to find the ZZZ node in network");
 
-    let steps = get_steps(start_id, end_id, instrs, network)?;
+    let steps = get_steps(start_id, &[end_id], instrs, network)?[0].1;
 
     writeln!(io::stdout(), "Part 1: {steps}")?;
     writeln!(io::stdout(), "> Time elapsed is: {:?}", start.elapsed())?;
@@ -112,6 +141,7 @@ fn part1(
 
 // https://zh.wikipedia.org/wiki/%E6%9C%80%E5%A4%A7%E5%85%AC%E5%9B%A0%E6%95%B8
 fn gcd(a: usize, b: usize) -> usize {
+    let (a, b) = (a.max(b), b.min(a));
     if b == 0 {
         a
     } else {
@@ -124,34 +154,69 @@ fn lcm(a: usize, b: usize) -> usize {
     a * b / gcd(a, b)
 }
 
-fn part2(
-    instrs: &[char],
-    network: &HashMap<NodeId, NextNode>,
-    map: &HashMap<String, NodeId>,
-) -> Result<usize> {
+fn dfs(
+    start_to_end_steps: &HashMap<usize, Vec<(usize, usize)>>,
+    start_count: usize,
+    end_count: usize,
+    visited_start: &mut HashSet<usize>,
+    visited_end: &mut HashSet<usize>,
+) -> Option<usize> {
+    let mut min_steps = usize::MAX;
+    for (&start, end_steps) in start_to_end_steps.iter() {
+        if visited_start.insert(start) {
+            for &(end, steps) in end_steps {
+                if visited_end.insert(end) {
+                    if let Some(remain_steps) = dfs(
+                        start_to_end_steps,
+                        start_count,
+                        end_count,
+                        visited_start,
+                        visited_end,
+                    ) {
+                        min_steps = min_steps.min(lcm(steps, remain_steps));
+                    }
+                    visited_end.remove(&end);
+                }
+            }
+            visited_start.remove(&start);
+        }
+    }
+    if min_steps != usize::MAX {
+        Some(min_steps)
+    } else if visited_start.len() == start_count && visited_end.len() == end_count {
+        Some(1)
+    } else {
+        None
+    }
+}
+
+fn part2(instrs: &[char], network: &Network, map: &NodeMap) -> Result<usize> {
     let start = Instant::now();
 
-    let start_ids: HashSet<_> = map
+    let start_ids: Vec<_> = map
         .iter()
         .filter(|(k, _)| k.ends_with('A'))
         .map(|(_, v)| *v)
         .collect();
-    let end_ids: HashSet<_> = map
+    let end_ids: Vec<_> = map
         .iter()
         .filter(|(k, _)| k.ends_with('Z'))
         .map(|(_, v)| *v)
         .collect();
-    let mut steps_martix = vec![vec![0; end_ids.len()]; start_ids.len()];
+    let mut start_to_end_steps = HashMap::new();
 
-    dbg!(start_ids.len() * end_ids.len());
-
-    for (i, &start_id) in start_ids.iter().enumerate() {
-        for (j, &end_id) in end_ids.iter().enumerate() {
-            steps_martix[i][j] = get_steps(start_id, end_id, instrs, network).unwrap_or(1);
-        }
+    for &start in &start_ids {
+        start_to_end_steps.insert(start, get_steps(start, &end_ids, instrs, network)?);
     }
 
-    let steps = steps_martix.iter().flatten().fold(1, |l, s| lcm(l, *s));
+    let steps = dfs(
+        &start_to_end_steps,
+        start_ids.len(),
+        end_ids.len(),
+        &mut HashSet::new(),
+        &mut HashSet::new(),
+    )
+    .unwrap();
 
     writeln!(io::stdout(), "Part 2: {steps}")?;
     writeln!(io::stdout(), "> Time elapsed is: {:?}", start.elapsed())?;
