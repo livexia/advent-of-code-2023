d296ad4625d563159e9a108eaf89e71089fc6ac3
livexia <istillalive@live.cn>
2023-12-08 14:15:09 +0800

    day 8 part 2 with lcm and gcd

--- aoc08/src/main.rs
+++ aoc08/src/main.rs
@@ -67,6 +67,28 @@ fn move_node(instr: char, id: NodeId, network: &HashMap<NodeId, NextNode>) -> Re
     }
 }
 
+fn get_steps(
+    a: usize,
+    b: usize,
+    instrs: &[char],
+    network: &HashMap<NodeId, NextNode>,
+) -> Result<usize> {
+    let mut cur = a;
+    let mut cur_instr = 0;
+    let mut steps = 0;
+    let mut seen = HashSet::new();
+    while cur != b && seen.insert((cur, cur_instr)) {
+        cur = move_node(instrs[cur_instr], cur, network)?;
+        steps += 1;
+        cur_instr = (cur_instr + 1) % instrs.len();
+    }
+    if cur == b {
+        Ok(steps)
+    } else {
+        err!("There is a loop")
+    }
+}
+
 fn part1(
     instrs: &[char],
     network: &HashMap<NodeId, NextNode>,
@@ -81,18 +103,25 @@ fn part1(
         .get("ZZZ")
         .expect("Unable to find the ZZZ node in network");
 
-    let mut cur = start_id;
-    let mut cur_instr = 0;
-    let mut steps = 0;
-    while cur != end_id {
-        cur = move_node(instrs[cur_instr], cur, network)?;
-        steps += 1;
-        cur_instr = (cur_instr + 1) % instrs.len();
-    }
+    let steps = get_steps(start_id, end_id, instrs, network)?;
 
     writeln!(io::stdout(), "Part 1: {steps}")?;
     writeln!(io::stdout(), "> Time elapsed is: {:?}", start.elapsed())?;
     Ok(steps)
+}
+
+// https://zh.wikipedia.org/wiki/%E6%9C%80%E5%A4%A7%E5%85%AC%E5%9B%A0%E6%95%B8
+fn gcd(a: usize, b: usize) -> usize {
+    if b == 0 {
+        a
+    } else {
+        gcd(b, a % b)
+    }
+}
+
+// https://zh.wikipedia.org/zh-hans/%E6%9C%80%E5%B0%8F%E5%85%AC%E5%80%8D%E6%95%B8
+fn lcm(a: usize, b: usize) -> usize {
+    a * b / gcd(a, b)
 }
 
 fn part2(
@@ -102,7 +131,7 @@ fn part2(
 ) -> Result<usize> {
     let start = Instant::now();
 
-    let mut cur_ids: HashSet<_> = map
+    let start_ids: HashSet<_> = map
         .iter()
         .filter(|(k, _)| k.ends_with('A'))
         .map(|(_, v)| *v)
@@ -112,18 +141,17 @@ fn part2(
         .filter(|(k, _)| k.ends_with('Z'))
         .map(|(_, v)| *v)
         .collect();
+    let mut steps_martix = vec![vec![0; end_ids.len()]; start_ids.len()];
 
-    let mut cur_instr = 0;
-    let mut steps = 0;
-    while cur_ids != end_ids {
-        let instr = instrs[cur_instr];
-        cur_ids = cur_ids
-            .iter()
-            .map(|id| move_node(instr, *id, network))
-            .collect::<Result<HashSet<_>>>()?;
-        steps += 1;
-        cur_instr = (cur_instr + 1) % instrs.len();
+    dbg!(start_ids.len() * end_ids.len());
+
+    for (i, &start_id) in start_ids.iter().enumerate() {
+        for (j, &end_id) in end_ids.iter().enumerate() {
+            steps_martix[i][j] = get_steps(start_id, end_id, instrs, network).unwrap_or(1);
+        }
     }
+
+    let steps = steps_martix.iter().flatten().fold(1, |l, s| lcm(l, *s));
 
     writeln!(io::stdout(), "Part 2: {steps}")?;
     writeln!(io::stdout(), "> Time elapsed is: {:?}", start.elapsed())?;
@@ -176,5 +204,5 @@ fn real_input() {
     let input = std::fs::read_to_string("input/input.txt").unwrap();
     let (instrs, network, map) = parse_input(&input).unwrap();
     assert_eq!(part1(&instrs, &network, &map).unwrap(), 17287);
-    assert_eq!(part2(&instrs, &network, &map).unwrap(), 17287);
+    assert_eq!(part2(&instrs, &network, &map).unwrap(), 18625484023687);
 }
